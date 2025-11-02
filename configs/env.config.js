c2d7a0109182d839582ca58f8ca311b5f00e4988
abhilashmadi <109837171+abhilashmadi@users.noreply.github.com>
2025-11-02 10:21:51 +0530

    feat: added better auth github required vars

--- configs/env.config.js
+++ configs/env.config.js
@@ -6,14 +6,25 @@ const DEV_MODES = {
 	TEST: 'testing',
 };
 
-const envSchema = z.object({
+export const envSchema = z.object({
 	// Server Configuration
-	PORT: z.coerce.number().positive(),
+	PORT: z.coerce.number().int().positive().describe('Port number on which the server runs'),
+
 	MODE: z.enum(Object.values(DEV_MODES)),
 
 	// Database Configuration
-	MONGO_URI: z.url().startsWith('mongodb').describe('MongoDB connection URI'),
-	DB_NAME: z.string(),
+	MONGO_URI: z.url().startsWith('mongodb', { message: 'Must be a valid MongoDB URI' }).describe('Full MongoDB connection string'),
+
+	DB_NAME: z.string().min(1, { message: 'Database name cannot be empty' }).describe('MongoDB database name'),
+
+	// Authentication Configuration
+	BETTER_AUTH_URL: z.url().describe('Base URL for the BetterAuth service'),
+
+	BETTER_AUTH_SECRET: z.string().min(1, { message: 'BetterAuth secret is required' }).describe('Secret key for BetterAuth integration'),
+
+	GITHUB_CLIENT_ID: z.string().min(1, { message: 'GitHub Client ID is required' }).describe('OAuth client ID for GitHub login'),
+
+	GITHUB_CLIENT_SECRET: z.string().min(1, { message: 'GitHub Client Secret is required' }).describe('OAuth client secret for GitHub login'),
 });
 
 export const envConfig = envSchema.parse(process.env);
